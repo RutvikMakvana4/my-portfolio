93b6c13c258eed72dd468a4a8e792d4a55bbb65a
RutvikMakvana4 <rutvikmakvana432@gmail.com>
2024-03-16 12:51:22 +0530

    feat: add navbar and banner page

---
+++ src/components/features/Features.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Features = () => {
+    return (
+        <section
+            id="features"
+            className="w-full h-[800px] pb-20 flex border-b-[1px] font-titleFont border-b-black"
+        >
+            Features
+
+        </section>
+    )
+}
+
+export default Features
