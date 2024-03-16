caaacc66a79dcf08439e4a1ed706076e3b4e6a62
RutvikMakvana4 <rutvikmakvana432@gmail.com>
2024-03-16 14:22:53 +0530

    feat: add contact form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Banner from './components/banner/Banner'
+import Contact from './components/contact/Contact'
 import Features from './components/features/Features'
 import Navbar from './components/navbar/Navbar'
 
@@ -7,11 +8,12 @@ function App() {
 
   return (
     <>
-      <div className="w-full h-auto bg-bodyColor text-lightText">
-        <div className="max-w-screen-xl mx-auto px-16">
+      <div className="w-full h-auto bg-bodyColor text-lightText px-4">
+        <div className="max-w-screen-xl mx-auto">
           <Navbar />
           <Banner />
           <Features />
+          <Contact />
         </div>
       </div>
     </>
