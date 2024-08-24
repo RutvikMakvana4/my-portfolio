a560c54553b3c51cf7d9f58ad86bb9c13d2514cf
RutvikMakvana4 <rutvikmakvana432@gmail.com>
2024-08-24 14:16:22 +0530

    feat: add new twitter logo

--- src/components/banner/Media.jsx
+++ src/components/banner/Media.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
-import { FaGithub, FaTwitter, FaLinkedinIn } from "react-icons/fa";
-import { socialMedia } from '../../utils/socialMedia';
+import React from "react";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { socialMedia } from "../../utils/socialMedia";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Media = () => {
   return (
     <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
       <div>
-        <h2 className="text-base uppercase font-titleFont mb-4">
-          Find me in
-        </h2>
+        <h2 className="text-base uppercase font-titleFont mb-4">Find me in</h2>
         <div className="flex gap-4">
           {socialMedia.map((item, index) => (
-            <a key={index} href={item.href} target="_blank" rel="noopener noreferrer" className="bannerIcon">
-              {item.title === 'github' && <FaGithub />}
-              {item.title === 'twitter' && <FaTwitter />}
-              {item.title === 'linkedin' && <FaLinkedinIn />}
+            <a
+              key={index}
+              href={item.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bannerIcon"
+            >
+              {item.title === "github" && <FaGithub />}
+              {item.title === "twitter" && <FaXTwitter />}
+              {item.title === "linkedin" && <FaLinkedinIn />}
             </a>
           ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Media
+export default Media;
--- src/components/contact/ContactLeft.jsx
+++ src/components/contact/ContactLeft.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { FaGithub, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import React from "react";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { contactImg } from "../../assets/index";
-import { socialMedia } from '../../utils/socialMedia';
+import { socialMedia } from "../../utils/socialMedia";
+import { FaXTwitter } from "react-icons/fa6";
 
 const ContactLeft = () => {
   return (
@@ -13,33 +14,40 @@ const ContactLeft = () => {
       />
       <div className="flex flex-col gap-4">
         <h3 className="text-3xl font-bold text-white">Rutvik Makvana</h3>
-        <p className="text-lg font-normal text-gray-400">
-          Node Js Developer
-        </p>
+        <p className="text-lg font-normal text-gray-400">Node Js Developer</p>
         <p className="text-base text-gray-400 tracking-wide">
-          💻 Passionate full-stack developer dedicated to crafting impactful digital solutions. Let's collaborate and build something extraordinary!
+          💻 Passionate full-stack developer dedicated to crafting impactful
+          digital solutions. Let's collaborate and build something
+          extraordinary!
         </p>
         <p className="text-base text-gray-400 flex items-center gap-2">
           Phone: <span className="text-lightText">+91 6355423641</span>
         </p>
         <p className="text-base text-gray-400 flex items-center gap-2">
-          Email: <span className="text-lightText">rutvikmakvana432@gmail.com</span>
+          Email:{" "}
+          <span className="text-lightText">rutvikmakvana432@gmail.com</span>
         </p>
       </div>
       <div className="flex flex-col gap-4">
         <h2 className="text-base uppercase font-titleFont mb-4">Find me in</h2>
         <div className="flex gap-4">
           {socialMedia.map((item, index) => (
-            <a key={index} href={item.href} target="_blank" rel="noopener noreferrer" className="bannerIcon">
-              {item.title === 'github' && <FaGithub />}
-              {item.title === 'twitter' && <FaTwitter />}
-              {item.title === 'linkedin' && <FaLinkedinIn />}
+            <a
+              key={index}
+              href={item.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bannerIcon"
+            >
+              {item.title === "github" && <FaGithub />}
+              {item.title === "twitter" && <FaXTwitter />}
+              {item.title === "linkedin" && <FaLinkedinIn />}
             </a>
           ))}
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default ContactLeft
+export default ContactLeft;
